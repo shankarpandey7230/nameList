bd1d6be1fb475c32ae4c58a02a8b699ed8e305e1
shankarpandey7230 <shankarpandey7230@gmail.com>
2024-05-30 09:54:41 +1000

    completing html and css adding controlled components concept

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('');
+
+  const handleOnChange = (e) => {
+    // console.log(e.target.value);
+    const { value } = e.target;
+    // console.log(value);
+    setName(value);
+  };
+
   return (
-    <div className="App">
-      <input type="text" class="" />
+    <div
+      className="wrapper d-flex justify-content-center align-items-center fs-2 "
+      style={{ height: '100vh' }}
+    >
+      <div className="userList shadow-lg p-3 mb-5 bg-body rounded p-4">
+        <div className="display">{name}</div>
+        <div className="form">
+          <form action="">
+            <input type="text" onChange={handleOnChange} />
+            <button>Add User</button>
+          </form>
+          <hr />
+        </div>
+        <div className="list">
+          <ul>
+            <li>Shankar</li>
+            <li>Pandey</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
